5c671a79e42143c9670fb061c5f945873b72c1fa
Napawit <napawit.to@tnis.com>
2019-10-11 14:26:12 +0700

    Update k8s.sh

--- k8s.sh
+++ k8s.sh
@@ -1,20 +1,12 @@
 #!/bin/bash
-sudo apt-get update
-sudo apt install -y \
-    apt-transport-https \
-    ca-certificates \
-    curl \
-    software-properties-common
-
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg |\
+sudo apt update
+sudo apt install -y apt-transport-https
+curl -s \
+    https://packages.cloud.google.com/apt/doc/apt-key.gpg |\
     sudo apt-key add -
-
-sudo add-apt-repository \
-    "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
-    $(lsb_release -cs) \
-    stable"
-   
-sudo apt-get update
-sudo apt-get -y install docker-ce
-sudo usermod -aG docker $USER
-sudo systemctl enable docker
+sudo touch /etc/apt/sources.list.d/kubernetes.list
+echo \
+    "deb http://apt.kubernetes.io/ kubernetes-xenial main" |\
+    sudo tee -a /etc/apt/sources.list.d/kubernetes.list
+sudo apt update
+sudo apt install -y kubeadm=1.15.0-00  kubelet=1.15.0-00 kubectl=1.15.0-00
